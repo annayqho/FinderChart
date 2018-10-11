5405406cfd1a852ddc9ee6b9627ac7e3728c25f4
annayqho <annayqho@gmail.com>
2018-10-10 18:26:16 -0700

    initial commit

---
+++ finder.py
@@ -0,0 +1,65 @@
+""" Finder chart script, using ztfquery """
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from astropy.time import Time
+from ztfquery import query,marshal
+
+# Name of target
+name = "ZTF18abkmbpy"
+
+# Get position of target
+m = marshal.MarshalAccess(auth=['annayqho', 'd1ewdw11z_growth'])
+m.load_target_sources()
+coords = m.get_target_coordinates(name)
+ra = coords.ra.values[0]
+dec = coords.dec.values[0]
+
+# Get light curve of target
+m.download_lightcurve(name) # dirout option doesn't seem to work?
+out_dir = "Data/marshal/lightcurves/%s/" %name
+lc_dict = pd.read_csv(out_dir + "marshal_lightcurve_%s.csv" %name)
+
+# Get metadata of all images at this location
+zquery = query.ZTFQuery()
+zquery.load_metadata(
+        radec=[ra,dec], size=0.01,
+        auth=['ah@astro.caltech.edu', 'd1ewdw11z_IRSA'])
+out = zquery.metatable
+
+# Do you need to use a reference image?
+need_ref = ndet == 0
+
+# If there are none, then use a reference image
+if need_ref:
+    zquery.load_metadata(kind="ref",radec=[ra, dec], size=0.0001)
+    out = zquery.metatable
+
+# Count the number of detections where limmag > 19.5
+# If 0, allow limmag > 19
+limmag = out.maglimit.values
+limmag_val = 19.5 # must be deeper than this value
+choose = limmag > limmag_val
+while sum(choose) == 0:
+    limmag_val += 0.5
+    choose = limmag > limmag_val
+im_ind = np.argmax(limmag[choose])
+out[choose][im_ind] # index of the image you choose
+
+
+# Construct a light curve at this position
+
+
+# Among those images, choose the most recent one in time
+all_jd = out[choose].obsjd.values
+indx = np.argmin(np.abs(all_jd-t.jd))
+out[choose].values[indx]
+
+# obs closest in time to the detection
+# choose = np.argmin(np.abs(all_jd-t.jd))
+# metadata corresponding to that observation
+# zquery.load_metadata(
+#         radec=[ra,dec], size=0.01, sql_query="obsjd=%s" %all_jd[choose])
+# out = zquery.metatable
+#zquery.download_data("psfcat.fits", show_progress=True, overwrite=True)
